e88860f21046ca961572d91fc6669db1dd4aaff8
sandy99405 <sandysandilya99@gmail.com>
2023-03-31 06:06:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from './FirebaseConfig';
+import './App1.css'
+
+function App() {
+  return (
+    <div className="App">
+        <div className="title-row">
+          <h1 className="title">Firebase Recipes</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
