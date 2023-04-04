37ac2a8effc09d0d153b2bef690e4dc582a67a29
sandy99405 <sandysandilya99@gmail.com>
2023-04-04 06:32:31 +0530

    added CD7

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,5 +1,5 @@
-import {initializeApp} from "firebase/app";
-import {getAuth,GoogleAuthProvider} from "firebase/auth";
+/* import {initializeApp} from "firebase/app";
+import {getAuth,GoogleAuthProvider} from "firebase/auth"; */
 import firebase from 'firebase/compat/app'
 /* import {getFirestore} from "firebase/firestore"; */
 
