a26de8a1afbe73883c33767d8647c5590fa4f305
sandy99405 <sandysandilya99@gmail.com>
2023-04-04 06:10:29 +0530

    added CD3

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,13 +3,13 @@ import {getAuth,GoogleAuthProvider} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const config ={
-  apiKey: Process.env.REACT_APP_API_KEY, 
-  authDomain: Process.env.REACT_APP_AUTH_DOMAIN, 
-  projectId: Process.env.REACT_APP_PROJECT_ID, 
-  storageBucket: Process.env.REACT_APP_STORAGE_BUCKET, 
-  messagingSenderId: Process.env.REACT_APP_MESSAGING_SENDER_ID, 
-  appId: Process.env.REACT_APP_APP_ID, 
-  measurementId: Process.env.REACT_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY, 
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN, 
+  projectId: process.env.REACT_APP_PROJECT_ID, 
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET, 
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, 
+  appId: process.env.REACT_APP_APP_ID, 
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
 
 const app = initializeApp(config);
