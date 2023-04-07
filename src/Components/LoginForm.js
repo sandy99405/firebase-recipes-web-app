fd5c765e4aa74d73c48fc5bda03f2daee185963b
sandy99405 <sandysandilya99@gmail.com>
2023-04-07 07:43:52 +0530

    final add

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -82,7 +82,6 @@ function LoginForm({existingUser}){
                     <button className="primary-button">Login</button>
                     <button type="button" onClick={handleSendResetPasswordEmail} className="primary-button">Reset</button>
                     <button type="button" onClick={handleLoginWithGoogle} className="primary-button">Login with Google!</button>
-                    <button>Hello button</button>
                  </div>
              </form>
           }
